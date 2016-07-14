af337562b708b3b470ef07188a0ba7bdc8ba652c
zhekamegarep <zhekamegarep@mail.ru>
2016-07-14 22:30:04 +0300

    Добавление блокировки (RO не работает)
    ...*(Добавить ограничение доступа при блокировке)

---
+++ www/pages/giveban.php
@@ -0,0 +1,46 @@
+<?php
+
+session_start();
+if($_SESSION['auth'] == yes)
+{
+
+if(isset($_POST['submit']))
+{    
+$login = $_POST['login'];
+$ban = $_POST['status'];
+
+    $connect = mysql_connect('localhost','root','1234567k') or die(mysql_error());
+mysql_select_db('testsite_users');
+$query = mysql_query("UPDATE users SET status = '$ban' WHERE login = '$login';") or die("Ошибка в записи, повторите попытку позже!");
+    $true = "yes";
+    $access = true;
+    }
+}
+else
+{
+    $access = false;
+}
+    ?>
+    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Страница пользователя <?php echo $_SESSION['login']; ?> TestSite.loc</title>
+<link href="http://testsite.loc/styles/toadminstyle.css" type="text/css" rel="stylesheet">
+</head>
+<?php 
+if($access = false)
+{
+goto noauth;
+} ?>
+<body>
+<?php if($true != "yes") { goto noauth; } ?>
+<p> <?php echo "Статус успешно изменён!"; ?> </p>
+<?php noauth: ?>
+<p><?php  if($true != "yes") { echo "\nУ Вас не достаточно прав для просмотра данного раздела";} ?></p>
+<br>
+<p class="back"><a href="http://testsite.loc">Вернуться на главную страницу</a></p>
+<br><br><br>
+<p class="copyright">©zhekamegarep | 2016 год</p>
+</body>
+</html>
